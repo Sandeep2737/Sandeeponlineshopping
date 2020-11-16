7cd9cd23934ff97db889f934d32c17f5100057d1
Sandeep2737 <lankasandeep808@gmail.com>
2020-11-16 17:52:33 +0530

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {Router} from '@angular/router';
+
+ 
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm:FormGroup;
+ 
+  constructor(private router:Router) {
+    this.loginForm=new FormGroup({
+      mailid:new FormControl(null,[Validators.required,Validators.email]),
+      password:new  FormControl(null,Validators.required)
+    });
+   }
+   get UserName()
+  {
+    return this.loginForm.get('UserName')
+  }
+
+  get Password()
+  {
+    return this.loginForm.get('Password')
+  }
+ err;
+   doLogin()
+   {
+     
+    if(this.UserName.value=="Admin" && this.Password.value=="admin")
+    {
+         this.router.navigate(['admin']);
+       
+       //  localStorage.setItem('email',this.mailid.value)
+       sessionStorage.setItem('User Name',this.UserName.value)
+    }
+      else if(this.UserName.value=="HarikaKaja" && this.Password.value=="123")
+       {
+            this.router.navigate(['Booking']);
+          
+            sessionStorage.setItem('User Name',this.UserName.value)
+       }
+       else
+       {
+         this.err="Invalid username or password!!";
+       }
+   }
+
+  ngOnInit()  {
+  }
+
+}
